d89ee5227a0a1d75fbfdd960bfcd5f5bd0a08499
florian <florian.tousnakhoff@gmail.com>
2022-05-01 12:15:00 +0200

    tableau dynamques pour les lignes

--- mini-vim/main.c
+++ mini-vim/main.c
@@ -1,22 +1,13 @@
 #include "../stdlib/stdlib.h"
 #include "stddef.h"
+#include "structures.h"
+
 
 #define VIDEO_W 80
 #define VIDEO_H 25
 
-char* buffer = 0; // Au départ on a un buffer vide
-
-typedef struct text{
-	char c;
-	uint8 fg; // La couleur du premier plan
-	uint8 bg; // La couleur de l'arriere plan
-	uint8 cursor; // Le curseur est-il dessus
-} text_t;
-
-
-typedef struct line{
-	text_t* line_buffer;
-}line_t;
+#define BUFF_SIZE 1000
+#define line_number;
 
 void* memset(void *pointer, int value, size_t count){
 	uint8 *p = pointer;
@@ -29,16 +20,16 @@ int main(){
 	// Donc déjà il faut lire un fichier (mais bon ça c'est pour plus tard
 	//
 	int line_under = 0; // la premiere ligne affichée à l'écran
-	text_t buffer[100000]; // Pour l'instant on a juste un buffer alloué n'importe comment, il faudra utiliser malloc
-	memset(buffer, 0, sizeof(text_t)*10000);
+	text_tab_t buffer[BUFF_SIZE]; // Pour l'instant on a juste un buffer alloué n'importe comment, il faudra utiliser malloc
+	memset(buffer, 0, sizeof(text_t)*BUFF_SIZE);
 	int cursorX = 0, cursorY = 0;
-	for(int i = 0; i < 100000; i ++){
-		buffer[i].c = ' ';
-		buffer[i].fg = WHITE;
-		buffer[i].bg = BLACK;
-		buffer[i].cursor = 0;
+	for(int i = 0; i < BUFF_SIZE; i ++){
+		text_t buff[80];
+		memset(buffer, 0, sizeof(text_t)*BUFF_SIZE);
 	}
 	buffer[0].cursor = 1;
+
+	mode_t current_mode = NORMAL;
 	
 	while(1){ // main loop
 		// On va afficher à l'écran le buffer
@@ -56,37 +47,65 @@ int main(){
 		// On a pas encore les gestions claviers
 		// Il faudrait plutot considerer ça ligne par ligne, parce que la on a le probleme que chaque ligne fait au plus 80 caracteres...
 		keyboard_t kp = get_keyboard();
-
-		if(kp.type == 0){ // On va faire un handler simple
-			switch (kp.k.ch){
-				case 'h': // On va à gauche
-					if(cursorX > 0) 
-					{
-						buffer[(cursorX--)+ (cursorY + line_under)*VIDEO_W].cursor = 0;  ;
-						buffer[(cursorX)+ (cursorY + line_under)*VIDEO_W].cursor = 1;  ;
+		switch (current_mode){
+			case NORMAL:
+				if(kp.type == 0){ // On va faire un handler simple
+					switch (kp.k.ch){
+						case 'h': // On va à gauche
+							if(cursorX > 0) 
+							{
+								buffer[(cursorX--)+ (cursorY + line_under)*VIDEO_W].cursor = 0;  ;
+								buffer[(cursorX)+ (cursorY + line_under)*VIDEO_W].cursor = 1;  ;
+							}
+							break;
+						case 'k': // On va en haut
+							if(cursorY > 0) 
+							{
+								buffer[(cursorX)+ ((cursorY--) + line_under)*VIDEO_W].cursor = 0;  ;
+								buffer[(cursorX)+ (cursorY + line_under)*VIDEO_W].cursor = 1;  ;
+							}
+							break;
+						case 'j': // On va en bas
+							buffer[(cursorX)+ ((cursorY++) + line_under)*VIDEO_W].cursor = 0;  ;
+							buffer[(cursorX)+ (cursorY + line_under)*VIDEO_W].cursor = 1;  ;
+							break;
+						case 'l': // On va à droite
+							if(cursorX < VIDEO_W -1) 
+							{
+								buffer[(cursorX++)+ (cursorY + line_under)*VIDEO_W].cursor = 0;  ;
+								buffer[(cursorX)+ (cursorY + line_under)*VIDEO_W].cursor = 1;  ;
+							}
+							break;
+						case 'i': // On passe en insert mode
+							current_mode = INSERT;
+							break;
 					}
-					break;
-				case 'k': // On va en haut
-					if(cursorY > 0) 
-					{
-						buffer[(cursorX)+ ((cursorY--) + line_under)*VIDEO_W].cursor = 0;  ;
-						buffer[(cursorX)+ (cursorY + line_under)*VIDEO_W].cursor = 1;  ;
-					}
-					break;
-				case 'j': // On va en bas
-					buffer[(cursorX)+ ((cursorY++) + line_under)*VIDEO_W].cursor = 0;  ;
-					buffer[(cursorX)+ (cursorY + line_under)*VIDEO_W].cursor = 1;  ;
-					break;
-				case 'l': // On va à droite
-					if(cursorX < VIDEO_W -1) 
-					{
-						buffer[(cursorX++)+ (cursorY + line_under)*VIDEO_W].cursor = 0;  ;
-						buffer[(cursorX)+ (cursorY + line_under)*VIDEO_W].cursor = 1;  ;
-					}
-					break;
+				break;
 			}
+			case INSERT:
+				if(kp.type == 0 && kp.k.ch != 0){
+					buffer[(cursorX)+ (cursorY + line_under)*VIDEO_W].c = kp.k.ch; 
+					buffer[(cursorX++)+ (cursorY + line_under)*VIDEO_W].cursor = 0;  
+					buffer[(cursorX)+ (cursorY + line_under)*VIDEO_W].cursor = 1;  
+					if(cursorX >= VIDEO_W){
+						cursorX =0;
+						cursorY++;
+					}
+					if(cursorY == VIDEO_H){
+						line_under++;
+						cursorY--;
+					}
+				}
+				else{
+					if(kp.k.sp == ESCAPE){
+						current_mode = NORMAL;
+					}
+				}
 
-		}
+			break;
+
+
+			}
 
 
 	}
