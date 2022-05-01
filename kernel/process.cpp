a170c96ec774982706aaa4d0678f3186be2723a7
felix-lebrat <felix.lebrat@gmail.com>
2022-05-01 15:43:42 +0200

    pre pull

--- kernel/process.cpp
+++ kernel/process.cpp
@@ -21,7 +21,42 @@ void init_process_tab()
     }
 }
 
-extern uint32 nframes;
+// void charger_prog()
+// {
+    // Ata_fat_system afs(hda);
+    // if(!afs.is_ready())
+    // {
+        // print_string("erreur disque\n");
+        // while(1);
+    // }
+// 
+    // Fat_infos infos=fat_init(&afs);
+    // Fat_entry entries[10];
+    // int nb_entry=infos.root_fat_entry.read_entries(entries,10,&infos,&afs);
+    // int i;
+    // for(i=0;i<nb_entry;i++)
+    // {
+        // if(strcmp(entries[i].get_name(),"MVIM"))
+				// {
+						// print_string("Programme MVIM charge");
+            // break;
+				// }
+    // }
+    // if(i==nb_entry)
+    // {
+        // print_string("erreur prog non trouve\n");
+        // while(1);
+    // }
+    // 
+    // uint32 size=entries[i].get_size();
+    // uint32 cluster_count=(size+infos.byte_per_cluster-1)/infos.byte_per_cluster;
+    // uint8 buff[cluster_count*infos.byte_per_cluster];
+    // entries[i].read_data(buff,cluster_count,&infos,&afs);
+    // uint32 pid=load_process(buff);
+    // print_string("programme charge\n");
+    // run_process(pid);
+// }
+
 
 uint32 load_process(uint8* elf,uint32 current_pid)
 {
--- kernel/process.h
+++ kernel/process.h
@@ -14,6 +14,20 @@ typedef enum proc_state
     WAITING,RUNNABLE,ZOMBIE,FREE
 } proc_state;
 
+const uint32 max_path_length=100;
+const uint32 max_opened_files=10;
+
+typedef enum file_type
+{
+    STDIN,STDOUT,FAT_ENTRY
+} file_type;
+
+typedef struct file_desc
+{
+    file_type type;
+    Fat_entry entry;
+} file_desc;
+
 typedef struct process
 {
     uint32 ppid;
@@ -21,6 +35,9 @@ typedef struct process
     registers_t saved_context;
     uint32 pg_entry;
     page_directory_t *directory;
+    char path[max_path_length];
+    Fat_entry current_dir;
+    file_desc opened_files[max_opened_files];
 } process;
 
 void init_process_tab();
